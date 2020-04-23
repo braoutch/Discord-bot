d33170cef00569edbb8b9d5458f06b5960663f71
braoutch <antoinelassagne@gmail.com>
2020-04-23 17:24:00 +0200

    structure

--- bot.js
+++ bot.js
@@ -1,33 +1,39 @@
 const Discord = require("discord.js")
 const TokenFile = require("./auth.json")
 var token = TokenFile.token
-
+import GameClient from './gameclient'
 
 const bot = new Discord.Client()
-
+const gameclient = new GameClient()
+var isPlaying = false
 bot.login(token)
 
 bot.on("message", msg => {
-  if (!msg.content.startsWith("Waiting for")) {
-msg.channel.send("Waiting for @ColasV...")
- }
-})
+  if (msg.content.startsWith("Waiting for")) {
+    msg.channel.send("Waiting for @ColasV...")
+  }
 
-bot.on('voiceStateUpdate', (oldMember, newMember) => {
-  let newUserChannel = newMember.voiceChannel
-  let oldUserChannel = oldMember.voiceChannel
+  if (msg.content === "play") {
+    msg.channel.send("New game started ! to respond, type r: and to stop the game, end type gameover.")
+    isPlaying = true
+    gameclient.NewGame(msg.channel)
+  }
 
-
-  if(oldUserChannel === undefined && newUserChannel !== undefined) {
-
-     // User Joins a voice channel
-message.channel.send("Waiting for colavay...", {
- tts: true
-})
-
-  } else if(newUserChannel === undefined){
-
-    // User leaves a voice channel
+  if (isPlaying) {
+    if (msg.content === "gameover") {
+      msg.channel.send("New game started ! to respond, type r: and to stop the game, end type gameover.")
+      gameclient.GameOver(msg.channel)
+    }
+    if (msg.content.startsWith("r:")) {
+      gameclient.SendResponse(msg)
+    }
 
   }
+
+
+
+
+
+
+
 })
---
+++ gameClient.js
@@ -0,0 +1,16 @@
+export default class GameClient {
+    NewGame(channel)
+    {
+
+    }
+
+    GameOver(channel)
+    {
+
+    }
+
+    SendResponse(msg)
+    {
+        
+    }
+}
