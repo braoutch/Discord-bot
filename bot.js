de9bdd2205d8037b83e64ec7db8d467b06155ae6
braoutch <antoinelassagne@gmail.com>
2020-04-23 20:52:47 +0200

    WIP
    ON y est bientôt

--- bot.js
+++ bot.js
@@ -1,12 +1,17 @@
 const Discord = require("discord.js")
 const TokenFile = require("./auth.json")
 var token = TokenFile.token
-const GameClient = require ("./gameClient.js")
+const GameClient = require("./gameClient.js")
 
 const bot = new Discord.Client()
-const gameclient = new GameClient()
 var isPlaying = false
 bot.login(token)
+
+const https = require('https')
+var io = require('socket.io-client')
+var socket = io('http://localhost:8080')
+
+const gameclient = new GameClient(socket)
 
 bot.on("message", msg => {
   if (msg.content.startsWith("Waiting for") && msg.content != "Waiting for ColasV...") {
@@ -29,11 +34,49 @@ bot.on("message", msg => {
     }
 
   }
+})
 
+socket.on('dclc', function (message) {
+  console.log("Message from DCLC : " + message)
+})
 
+socket.on('GameAvailable', function (message) {
+  if (isPlaying) {
+    console.log("Game available !")
+    //socket.emit('message', "Ill take this game.");
 
+    const GetSetRequest = {
+      //hostname: 'dcdl-backend.azurewebsites.net',
+      hostname: 'localhost:8080',
+      port: 443,
+      path: '/sets/' + message,
+      method: 'GET'
+    }
 
+    const req = https.request(GetSetRequest, res => {
+      console.log(`statusCode: ${res.statusCode}`)
 
+      res.on('data', d => {
+        {
+          console.log(d)
 
+          if (d.mode === "numbers") {
+            let target = d.question.split(",")[0]
+            let numbers = d.question.split(",").shift().toString()
+            channel.send("New set of numbers " + numbers + ". The target is " + target + " ! ")
+          }
+          else if (d.mode === "letters") {
+            channel.send("New set of letters : " + d.question)
+          }
+        }
+      })
+    })
 
+    req.on('error', error => {
+      console.error(error)
+    })
+
+    req.end()
+
+  }
 })
--- gameClient.js
+++ gameClient.js
@@ -1,64 +1,15 @@
-const https = require('https')
-var io = require('socket.io-client')
-const SendReponseRequest = {
-    //hostname: 'dcdl-backend.azurewebsites.net',
-    hostname: 'localhost:8080',
-    port: 443,
-    path: '/actions',
-    method: 'GET'
-}
+// const https = require('https')
+// var io = require('socket.io-client')
+var socket
 
 class GameClient {
-    constructor() {
-        this.socket = io('http://localhost:8080')
-        console.log("Socket object created.")
-    }
+constructor(s){
+socket = s
+}
     NewGame(channel) {
-        this.socket.emit("I want to play.")
-
-        this.socket.on('error', function (message) {
-            console.log("Error : " + error)
-        })
-
-        this.socket.on('GameAvailable', function (message) {
-            console.log("Game available !")
-            this.socket.emit('message', "Ill take this game.");
-
-            const GetSetRequest = {
-                //hostname: 'dcdl-backend.azurewebsites.net',
-                hostname: 'localhost:8080',
-                port: 443,
-                path: '/sets/' + message,
-                method: 'GET'
-            }
-
-            const req = https.request(options, res => {
-                console.log(`statusCode: ${res.statusCode}`)
-
-                res.on('data', d => {
-                    {
-                        console.log(d)
-
-                        if (d.mode === "numbers") {
-                            let target = d.question.split(",")[0]
-                            let numbers = d.question.split(",").shift().toString()
-                            channel.send("New set of numbers " + numbers + ". The target is " + target + " ! ")
-                        }
-                        else if (d.mode === "letters") {
-                            channel.send("New set of letters : " + d.question)
-                        }
-                    }
-                })
-            })
-
-            req.on('error', error => {
-                console.error(error)
-            })
-
-            req.end()
-        })
+        socket.emit("I want to play.")
         for (const [memberId, member] of channel.members) {
-            this.socket.emit('message', "discord" + memberId, "");
+            socket.emit('message', "discord" + memberId + ";1234;" + channel.id)
         }
     }
 
@@ -66,7 +17,13 @@ class GameClient {
     }
 
     SendResponse(msg) {
+        const SendReponseRequest = {
+            //hostname: 'dcdl-backend.azurewebsites.net',
+            hostname: 'localhost:8080',
+            port: 443,
+            path: '/actions',
+            method: 'GET'
+        }
     }
 }
-
 module.exports = GameClient 
