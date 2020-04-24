a5c54aeefbe6bfec3c839b6d7118ea81a1368f89
braoutch <antoinelassagne@gmail.com>
2020-04-24 21:48:28 +0200

    Bug fix

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var token = TokenFile.token
 const GameClient = require("./gameClient.js")
 
 const bot = new Discord.Client()
-var isPlaying = false
+var isPlaying
 bot.login(token)
 
 const https = require('https')
@@ -14,26 +14,37 @@ var socket = io('http://localhost:8080')
 
 const gameclient = new GameClient(socket)
 
+const GetSetRequest = {
+  //hostname: 'dcdl-backend.azurewebsites.net',
+  hostname: 'localhost',
+  port: 8080,
+  path: '',
+  method: 'GET',
+  rejectUnauthorized: false
+}
+
 bot.on("message", msg => {
   if (msg.content.startsWith("Waiting for") && msg.content != "Waiting for ColasV...") {
     msg.channel.send("Waiting for ColasV...")
   }
 
   if (msg.content === "play") {
-    msg.channel.send("New game started ! to respond, type r: and to stop the game, end type gameover.")
+    msg.channel.send("New game started ! to respond, type r: and to stop the game, to end type stop.")
     isPlaying = true
     gameclient.NewGame(msg.channel)
   }
 
-  if (isPlaying) {
-    if (msg.content === "gameover") {
-      msg.channel.send("New game started ! to respond, type r: and to stop the game, end type gameover.")
+  if (isPlaying) 
+  {
+    if (msg.content === "stop") 
+    {
+      msg.channel.send("It's over now.")
       gameclient.GameOver(msg.channel)
+      isPlaying = false
     }
     if (msg.content.startsWith("r:")) {
-      gameclient.SendResponse(msg)
+      gameclient.SendResponse(msg.channel.id, msg.author.id, msg.content.substring(2))
     }
-
   }
 })
 
@@ -41,37 +52,34 @@ socket.on('dclc', function (message) {
   console.log("Message from DCLC : " + message)
 })
 
-socket.on('GameAvailable', function (message) {
+socket.on('GameAvailable', function (setId, roomId) {
   if (isPlaying) {
     console.log("Game available !")
+    var channel = bot.channels.cache.get(roomId)
     //socket.emit('message', "Ill take this game.");
-
-    const GetSetRequest = {
-      //hostname: 'dcdl-backend.azurewebsites.net',
-      hostname: 'localhost',
-      port: 8080,
-      path: '/sets/' + message,
-      method: 'GET',
-      rejectUnauthorized:false
-    }
-    console.log("request sent to " + GetSetRequest.path)
+    GetSetRequest.path = "/sets/" + setId
+    //console.log("request sending to " + GetSetRequest.path)
 
     const req = http.request(GetSetRequest, res => {
-      console.log(`statusCode: ${res.statusCode}`)
+      //console.log(`statusCode: ${res.statusCode}`)
       res.setEncoding('utf8');
-      
+
       res.on('data', d => {
         {
           //console.log("THIS IS : " + d)
-          let response = JSON.parse(d)
-          if (response.mode === "numbers") {
-            let target = response.question.split(",")[0]
-            let numbers = response.question.split(",").shift().toString()
-            channel.send("New set of numbers " + numbers + ". The target is " + target + " ! ")
+          let set = JSON.parse(d)
+          if (set.mode === "numbers") {
+            let question = set.question
+            //console.log(question)
+            let target = question.split(",")[0]
+            let numbers = question.split(",")
+            numbers.shift()
+            channel.send("New set of numbers " + numbers.toString() + ". The target is " + target + " ! ")
           }
-          else if (response.mode === "letters") {
-            channel.send("New set of letters : " + response.question)
+          else if (set.mode === "letters") {
+            channel.send("New set of letters : " + set.question)
           }
+          gameclient.RegisterCurrentSet(roomId,set.setId)
         }
       })
     })
@@ -84,4 +92,43 @@ socket.on('GameAvailable', function (message) {
     req.end()
 
   }
+})
+
+socket.on('GameOver', function (setId, roomId) {
+  var channel = bot.channels.cache.get(roomId)
+  GetSetRequest.path = "/sets/" + setId
+  //console.log("request sending to " + GetSetRequest.path)
+
+  const req = http.request(GetSetRequest, res => {
+    //console.log(`statusCode: ${res.statusCode}`)
+    res.setEncoding('utf8');
+    var winners = []
+    var score
+
+    res.on('data', d => {
+      {
+        let set = JSON.parse(d)
+        console.log(set)
+        let players = set.playersInSet
+        for (var player of players) {
+          if (player.victory === true) {
+            var playerName = bot.users.cache.get(player.playerId);
+            winners.push(playerName)
+            score = player.score
+          }
+        }
+        if(winners.length>0)
+        channel.send("Winners : " + winners.toString() + " with a score of " + score)
+        else
+        channel.send("No one wins this ! ")
+      }
+    })
+  })
+
+  req.on('error', error => {
+    console.error("This is an error : " + error)
+  })
+  console.log(req.path)
+
+  req.end()
 })
--- gameClient.js
+++ gameClient.js
@@ -2,55 +2,69 @@ const https = require('https')
 const http = require('http')
 // var io = require('socket.io-client')
 var socket
+var currentSet = {}
 
 class GameClient {
-constructor(s){
-socket = s
-}
-    NewGame(channel) {
-        socket.emit("I want to play.")
-        for (const [memberId, member] of channel.members) {
-            socket.emit('message', "discord" + memberId + ";1234;" + channel.id)
+  constructor(s) {
+    socket = s
+  }
+  NewGame(channel) {
+    socket.emit("I want to play.")
+    for (const [memberId, member] of channel.members) {
+      socket.emit('playerconnection', memberId + ";1234;" + channel.id)
+    }
+  }
+
+  GameOver(channel) {
+    delete currentSet[channel.id]
+    console.log("The room " + channel.id + " should now be disconnected.")
+    socket.emit('gameover', channel.id)
+  }
+
+  SendResponse(roomId, playerId, response) {
+    const data = JSON.stringify(
+      {
+        "setId": currentSet[roomId],
+        "playerId": playerId,
+        "response": response
+      })
+
+    console.log("Received response from " + playerId)
+
+    const SendReponseRequest = {
+      //hostname: 'dcdl-backend.azurewebsites.net',
+      hostname: 'localhost',
+      port: 8080,
+      path: '/actions',
+      method: 'POST',
+      rejectUnauthorized: false,
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+      }
+    }
+
+    const req = http.request(SendReponseRequest, res => {
+      console.log(`statusCode: ${res.statusCode}`)
+      res.setEncoding('utf8');
+
+      res.on('data', d => {
+        {
+          console.log(d)
         }
-    }
+      })
+    })
 
-    GameOver(channel) {
-    }
+    req.on('error', error => {
+      console.error(error)
+    })
 
-    SendResponse(msg) {
-        const SendReponseRequest = {
-            //hostname: 'dcdl-backend.azurewebsites.net',
-            hostname: 'localhost',
-            port: 8080,
-            path: '/actions',
-            method: 'POST'
-        }
+    req.write(data)
+    req.end()
+  }
 
-
-        const req = https.request(SendReponseRequest, res => {
-            console.log(`statusCode: ${res.statusCode}`)
-      
-            res.on('data', d => {
-              {
-                console.log(d)
-      
-                if (d.mode === "numbers") {
-                  let target = d.question.split(",")[0]
-                  let numbers = d.question.split(",").shift().toString()
-                  channel.send("New set of numbers " + numbers + ". The target is " + target + " ! ")
-                }
-                else if (d.mode === "letters") {
-                  channel.send("New set of letters : " + d.question)
-                }
-              }
-            })
-          })
-      
-          req.on('error', error => {
-            console.error(error)
-          })
-      
-          req.end()
-    }
+  RegisterCurrentSet(roomId, setId) {
+    currentSet[roomId] = setId
+  }
 }
 module.exports = GameClient 
